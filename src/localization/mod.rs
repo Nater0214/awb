5100b867b6ace13b314e8e15186090a024154683
Nater0214 <gamer@nater0214.com>
2025-09-23 21:10:35 -0400

    Improve localization invocation flexibility

--- src/localization/mod.rs
+++ src/localization/mod.rs
@@ -80,7 +80,7 @@ impl std::error::Error for LocalizationError {
 pub(crate) async fn localize_message(
     translation_key: impl AsRef<str>,
     language: impl AsRef<Language>,
-    args: &[impl ToString],
+    args: &[&(dyn ToString + Send + Sync)],
 ) -> Result<String, anyhow::Error> {
     // Convert arguments
     let translation_key = translation_key.as_ref().parse::<TranslationKey>()?;
@@ -124,10 +124,10 @@ pub(crate) async fn localize_message(
 #[macro_export]
 macro_rules! localize_message {
     ($key:expr, $lang:expr $(,)?) => {
-        crate::localization::localize_message($key, $lang, &[] as &[&str])
+        crate::localization::localize_message($key, $lang, &[])
     };
     ($key:expr, $lang:expr, $($args:expr),* $(,)?) => {
-        crate::localization::localize_message($key, $lang, &[$($args),*])
+        crate::localization::localize_message($key, $lang, &[$(&$args),*])
     };
 }
 
